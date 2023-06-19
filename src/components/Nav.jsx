6d6e6c3885c68f965c6205225a798cb158e2e792
EmyraldCorp <jesusboy1252@gmail.com>
2023-06-18 18:36:01 -0700

    added new components

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import './Nav.css'
 import logo from '../images/115168_45900_7388b824-b2f0-4ffa-ae56-58d13a637de8.jpg'
 
 function Nav() {
   return (
-    <div>
-      <img src={logo} alt="logo" className='img'/>
-      <ul>
-         <li>Home</li>
-         <li>About</li>
-         <li>Menu</li>
-         <li>Reservation</li>
-         <li>Order online</li>
-         <li>Login</li>
-      </ul>
+   <div className="container">
+      <div className='nav'>
+         <img src={logo} alt="logo" className='nav-img' />
+         <ul className='nav-item'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservation</li>
+            <li>Order online</li>
+            <li>Login</li>
+         </ul>
+      </div>
     </div>
   )
 }
